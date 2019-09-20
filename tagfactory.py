78ab57cdda28c43a3ac1c0bf8b0b581b3a51981e
Donovan Paget <donovan.p56@gmail.com>
2019-09-19 22:51:44 -0600

    more haha

--- tagfactory.py
+++ tagfactory.py
@@ -11,52 +11,52 @@ from __future__ import print_function
 import os, sys, csv
 from PIL import Image, ImageDraw, ImageFont
 im = Image.open("full tag.png")
-redlogo = Image.open("testred.png")
-orangelogo = Image.open("testorange.png")
-yellowlogo = Image.open("testyellow.png")
-greenlogo = Image.open("testgreen.png")
-bluelogo = Image.open("testblue.png")
-pinklogo = Image.open("testpink.png")
-logobox = (0,0,50,50)
-fontsize = 26
-fnt = ImageFont.truetype('arial.ttf',fontsize)
+burton = Image.open("logo/burtonlogo.png")
+jones = Image.open("logo/jones logo.png")
+k2 = Image.open("logo/k2logo.png")
+green = Image.open("testgreen.png")
+blue = Image.open("testblue.png")
+pink = Image.open("testpink.png")
+logolist = [[burton,"burton"], [jones, "jones"], [k2,"k2"], [green,"green"], [blue,"blue"], [pink,"pink"]]
+logobox = (0,0,480,555)
+fontsize = 160
+fontname = 'SairaCondensed-Regular.ttf'
+fnt = ImageFont.truetype(fontname,fontsize)
+
+
 with open("testbook.csv") as csv_file:
     csv_reader = csv.reader(csv_file,delimiter=',')
     for row in csv_reader:
         brand = row[0]
         item = row[1]
         price = row[2]
+        item = item.upper()
 
         # handling the logo based off what the brand category is
-        if brand == "red":
-            region = redlogo.crop(logobox)
-        if brand == "orange":
-            region = orangelogo.crop(logobox)
-        if brand == "yellow":
-            region = yellowlogo.crop(logobox)
-        if brand == "green":
-            region = greenlogo.crop(logobox)
-        if brand == "blue":
-            region = bluelogo.crop(logobox)
-        if brand == "pink":
-            region = pinklogo.crop(logobox)    
+        for company in logolist:
+            if company[1] == brand:
+                region = company[0].crop(logobox)  
         im.paste(region,logobox)
 
         # handling the item name
-        
         itemname = ImageDraw.Draw(im)
+        fontsize = 165
+        ycoord = 60
+
         # handling if the item name overflows off the tag
-        while itemname.textsize(item,font=fnt)[0] > 145: 
+        while itemname.textsize(item,font=fnt)[0] > 650: 
             fontsize = fontsize - 1
-            fnt = ImageFont.truetype('arial.ttf',fontsize)
-        itemname.text((55,0),item,font=fnt)
+            fnt = ImageFont.truetype(fontname,fontsize)
+            ycoord = ycoord + 0.5
+        itemname.text((518,ycoord),item,font=fnt)
         # reset font in case we messed with it to fix sizing!
-        fnt = ImageFont.truetype('arial.ttf',20)
+        fnt = ImageFont.truetype(fontname,165)
+        
 
         # handling the item price
         itemprice = ImageDraw.Draw(im)
-        itemprice.text((55,22),price,font=fnt)
+        itemprice.text((518,280),price,font=fnt)
 
-        fnt = ImageFont.truetype('arial.ttf',26)
+        # save the new tag
         im.save(brand+item+".png","PNG")
         im = Image.open("full tag.png")
